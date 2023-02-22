5b49e9428ab2f0780686b894a874194f32b0e901
Tem0B <Temo.baindurashvili@universaltransit.us>
2023-02-22 19:14:12 +0400

    add repositories

---
+++ GraphQL.Api/Repository/ProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using GraphQL.Api.Data;
+using GraphQL.Api.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GraphQL.Api.Repository
+{
+    public class ProductRepository
+    {
+        private readonly AppDbContext context;
+
+        public ProductRepository(AppDbContext context)
+        {
+            this.context=context;
+        }
+
+        public Task<List<Product>> GetAll()
+        {
+            return context.Products.ToListAsync();
+        }
+    }
+}
---
+++ GraphQL.Api/Repository/ProductReviewRepository.cs
@@ -0,0 +1,29 @@
+﻿
+using GraphQL.Api.Data;
+using GraphQL.Api.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GraphQL.Api.Repository
+{
+    public class ProductReviewRepository
+    {
+        private readonly AppDbContext _dbContext;
+
+        public ProductReviewRepository(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public List<ProductReview> GetForProduct(int productId)
+        {
+            var r = _dbContext.ProductReviews.Where(pr => pr.ProductId == productId).ToList();
+            return r;
+        }
+
+        public async Task<ILookup<int, ProductReview>> GetForProducts(IEnumerable<int> productIds)
+        {
+            var reviews = await _dbContext.ProductReviews.Where(pr => productIds.Contains(pr.ProductId)).ToListAsync();
+            return reviews.ToLookup(r => r.ProductId);
+        }
+    }
+}
