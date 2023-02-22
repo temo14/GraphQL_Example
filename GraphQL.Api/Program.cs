12f0807e5d16b9047aa1e2f0cfbb26e7dca898eb
Tem0B <Temo.baindurashvili@universaltransit.us>
2023-02-22 19:15:03 +0400

    add services configure program.cs

---
+++ GraphQL.Api/Program.cs
@@ -0,0 +1,39 @@
+using GraphQL;
+using GraphQL.Api.Data;
+using GraphQL.Api.GraphQL;
+using GraphQL.Api.Repository;
+using GraphQL.Server.Ui.Playground;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("AppDbContextConnection")));
+
+builder.Services.AddScoped<ProductRepository>();
+builder.Services.AddScoped<ProductReviewRepository>();
+builder.Services.AddScoped<GraphSchema>();
+
+builder.Services.AddGraphQL(o => o.
+    AddGraphTypes()
+    .AddSystemTextJson());
+
+var app = builder.Build();
+
+app.UseGraphQL<GraphSchema>();
+
+app.UseGraphQLPlayground(
+    "/ui/graphql",
+    new PlaygroundOptions
+    {
+        GraphQLEndPoint = "/graphql",
+        SubscriptionsEndPoint = "/graphql",
+        RequestCredentials = RequestCredentials.Include,
+    });
+
+using var scope = app.Services.CreateScope();
+var dbContext = scope.ServiceProvider.GetService<AppDbContext>();
+dbContext!.Database.EnsureCreated();
+dbContext.Seed();
+
+app.Run();
