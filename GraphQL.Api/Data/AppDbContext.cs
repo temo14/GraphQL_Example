8b1a4a0baa25a17301b0ed9681ce41c63f34d328
Tem0B <Temo.baindurashvili@universaltransit.us>
2023-02-22 19:13:31 +0400

    add Db and Data

---
+++ GraphQL.Api/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using GraphQL.Api.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GraphQL.Api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductReview> ProductReviews { get; set; }
+    }
+}
